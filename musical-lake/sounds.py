f18a25db9e15e05b93e37f1d215b4abd6d093bc1
Jorge Guerra Landázuri <jagl257@gmail.com>
2021-05-14 08:39:59 -0500

    Update sounds.py

--- musical-lake/sounds.py
+++ musical-lake/sounds.py
@@ -1,1 +1,19 @@
-Print ("Hello World")
+soundmap = {
+    'brr':"fiu",
+    'birip':"trri-trri",
+    'brrah':"null",
+    'croac':"null",
+    'fiu': "cric-cric",
+    'plop': "cric-cric",
+    'pep': "birip",
+    'cric-cric':"brrah",
+    'trri-trri':"croac",
+    'bri-bri': "plop",
+}
+
+def song(sound):
+    if soundmap.get(sound,"null") is "null":
+        return "\n"
+    else:
+        nextSound = soundmap.get(sound,"croac")
+        return (nextSound + ", " + song(nextSound))
