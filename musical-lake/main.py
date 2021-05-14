a36ae4ad0401669e03a5c977bc62cc9c06bdd6eb
Jorge Guerra Landázuri <jagl257@gmail.com>
2021-05-14 08:41:46 -0500

    Update main.py

--- musical-lake/main.py
+++ musical-lake/main.py
@@ -1,2 +1,9 @@
-import sounds.py as snd
+from sounds.py import soundmap, song
+
+print("Welcome to Musical Lake Program \n")
+print("For exiting, input the word \"end\" \n\n")
+sound = "init"
+while sound != "end":
+    sound = input("Input a sound: ")
+    print(song(sound))
 
